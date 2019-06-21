e0c9e6cd1ccee200b4eabad070d9d0a09a4d88cc
lwairore <kwairore@gmail.com>
2019-06-21 21:45:28 +0300

    Define new_image function that calls the NewImageForm.Pass in the request.FILES argument to NewImageForm.

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, redirect
-
+from .forms import NewImageForm
 
 # Create your views here.
-def home(request):
+def index(request):
     title = 'Instagram'
-    return render(request, 'suggestions.html', {'title':title})
+    return render(request, 'suggestions/suggestions.html', {'title':title})
+
+def new_image(request):
+    if request.method = 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.save()
+        else:
+            form = NewImageForm()
+    return render(request, 'image/new_image.html', {'form':form})
