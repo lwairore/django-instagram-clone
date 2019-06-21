6189ed75d2cfe3f91cb26d0a161caad9ab74775a
lwairore <kwairore@gmail.com>
2019-06-21 22:21:13 +0300

    Query all images with objects.all() method in index function

--- homepage/views.py
+++ homepage/views.py
@@ -1,17 +1,20 @@
 from django.shortcuts import render, redirect
 from .forms import NewImageForm
+from .models import Image
 
 # Create your views here.
 def index(request):
     title = 'Instagram'
+    images = Image.objects.all()
+    print(images)
     return render(request, 'suggestions/suggestions.html', {'title':title})
 
 def new_image(request):
-    if request.method = 'POST':
+    if request.method == 'POST':
         form = NewImageForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.save(commit=False)
             image.save()
-        else:
-            form = NewImageForm()
+    else:
+        form = NewImageForm()
     return render(request, 'image/new_image.html', {'form':form})
