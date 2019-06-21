1100e7d523918c692ca04daf799a1215fd1f4a5a
lwairore <kwairore@gmail.com>
2019-06-21 21:10:23 +0300

    Create class Image with image_name column and image_caption column.

---
+++ homepage/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    # image = models.CharField(max_)
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=30)
+    
