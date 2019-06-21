01477c17dacbea1da8d285ffdcf1b346fe48fd7b
lwairore <kwairore@gmail.com>
2019-06-21 21:18:36 +0300

    Add a new field to Image table that takes in the upload_to argument which defines where the image will be stored in the file system.

--- homepage/models.py
+++ homepage/models.py
@@ -5,4 +5,5 @@ class Image(models.Model):
     # image = models.CharField(max_)
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/')
     
