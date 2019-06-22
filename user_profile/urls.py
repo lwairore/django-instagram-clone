8c3fc254aeef98c4ce46dc0d6ac0634c18a72951
lwairore <kwairore@gmail.com>
2019-06-22 20:11:00 +0300

    Add edit route that calls edit_profile_page function.

---
+++ user_profile/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url, include
+from . import views
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    url('^edit/$', views.edit_profile_page),
+]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
