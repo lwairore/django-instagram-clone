64ef34af53a027139f7862837f5a8c0238fbaa20
lwairore <kwairore@gmail.com>
2019-06-22 20:11:44 +0300

    Define edit_profile_page that renders edit_profile_page.html.

--- user_profile/views.py
+++ user_profile/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+def edit_profile_page(request):
+    return render(request, 'profile/edit_user_profile_page.html')
+
+# def profile_page(request):
