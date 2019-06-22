d5c30ba04654adde61c234520099fb14d77d4dc2
lwairore <kwairore@gmail.com>
2019-06-22 14:48:17 +0300

    Import User class.Add uploaded_by column that is related to User.

--- homepage/models.py
+++ homepage/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Image(models.Model):
     # image = models.CharField(max_)
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
     image = models.ImageField(upload_to='images/', default='/home/karangu/Desktop/Instagram\ clone/media')
+    uploaded_by = models.ForeignKey(User, default=0)
+
     
     def save_image(self):
         self.save()
 
     def delete_image(self):
         self.delete()
+
+    
