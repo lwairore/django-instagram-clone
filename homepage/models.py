2f98cd454ec195f44768038a5334de31d1ae741e
lwairore <kwairore@gmail.com>
2019-06-22 10:01:19 +0300

    Create save_image() method that calls posrgres save() method

--- homepage/models.py
+++ homepage/models.py
@@ -5,5 +5,7 @@ class Image(models.Model):
     # image = models.CharField(max_)
     image_name = models.CharField(max_length=30)
     image_caption = models.CharField(max_length=30)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', default='/home/karangu/Desktop/Instagram\ clone/media')
     
+    def save_image(self):
+        self.save()
