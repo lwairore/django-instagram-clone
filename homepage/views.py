b6fd6a49b388f2ba126beae79576aaf0ba3247a3
lwairore <kwairore@gmail.com>
2019-06-22 23:11:08 +0300

    import NewImageFOrm, SignUpForm, HttpResponse. Create signup function and activate function.

--- homepage/views.py
+++ homepage/views.py
@@ -1,20 +1,79 @@
 from django.shortcuts import render, redirect
-from .forms import NewImageForm
+from .forms import NewImageForm, SignupForm
 from .models import Image
+from django.http import HttpResponse
+# from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from .tokens import account_activation_token
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def index(request):
     title = 'Instagram'
     images = Image.objects.all()
     print(images)
-    return render(request, 'suggestions/suggestions.html', {'title':title})
+    return render(request, 'suggestions/suggestions.html', {'title':title, 'images':images})
 
 def new_image(request):
     if request.method == 'POST':
         form = NewImageForm(request.POST, request.FILES)
+        current_user = request.user
         if form.is_valid():
             image = form.save(commit=False)
-            image.save()
+            image.uploaded_by = current_user
+            print('-' * 30)
+            print(image.uploaded_by)
+            image.save_image()
     else:
         form = NewImageForm()
     return render(request, 'image/new_image.html', {'form':form})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.save()
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your blog account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, to=[to_email]
+            )
+            email.send()
+            return HttpResponse('Please confirm your email address to complete the registration')
+    else:
+        form = SignupForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
