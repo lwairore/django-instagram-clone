7f61fa423c53244729d85d6531612f050017af2d
lwairore <kwairore@gmail.com>
2019-06-23 10:56:30 +0300

    Exclude comment table

--- homepage/forms.py
+++ homepage/forms.py
@@ -1,7 +1,17 @@
 from .models import Image
+
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = '__all__'
+        exclude = ['uploaded_by','comments']
+        # fields = '__all__'
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
