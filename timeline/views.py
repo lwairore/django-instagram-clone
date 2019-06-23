ac31a71553f1c2f3a17cba698ef76bc7655ceccd
lwairore <kwairore@gmail.com>
2019-06-23 10:00:50 +0300

    Import render, redirect from django.shortcuts. Import Image and Follow class from homepage.models. Import login_required from django.contrib.auth.decorators.

--- timeline/views.py
+++ timeline/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from homepage.models import Image, Follow
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login')
+def timeline_page(request):
+    pass
