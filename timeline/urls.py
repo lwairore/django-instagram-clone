1f44d411a90003fc2d9caaf97e29f84c219d4e13
lwairore <kwairore@gmail.com>
2019-06-25 11:02:53 +0300

    Create comment url patterb that directs the page to comment function. Create like url pattern that directs to like function.

--- timeline/urls.py
+++ timeline/urls.py
@@ -1,11 +1,12 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url('^$', views.timeline_page, name='timeline')
-
+    url('^$', views.timeline_page, name='timeline'),
+    url('^comment/(\d+)/$', views.comment, name="comment"),
+    url(r'^like/(\d+)/$', views.like, name='like'),
 
 ]
 
