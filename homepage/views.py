85fd721d4fc039b6942cf6c785275980165831f9
lwairore <kwairore@gmail.com>
2019-06-26 08:35:17 +0300

    Deployment to Heroku

--- homepage/views.py
+++ homepage/views.py
@@ -2,18 +2,10 @@ from django.shortcuts import render, redirect
 from .forms import NewImageForm, SignupForm
 from .models import Image, Follow
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 # from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
-from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.template.loader import render_to_string
-from .tokens import account_activation_token
-from django.contrib.auth.models import User
-from django.core.mail import EmailMessage
-from django.views.decorators.csrf import ensure_csrf_cookie
-
 
 # Create your views here.
 def home_intro(request):
@@ -56,42 +48,12 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = False
+            user.is_active = True
             user.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your blog account.'
-            message = render_to_string('acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-                'token':account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                        mail_subject, message, to=[to_email]
-            )
-            email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
+            return HttpResponse('<body style="background: #e2e2e2;">Now you can <a href="/accounts/login/">login</a> your account.</body>')
     else:
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
-
-
-def activate(request, uidb64, token):
-    try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        # return redirect('home')
-        return HttpResponse('<body style="background: #e2e2e2;">Thank you for your email confirmation. Now you can <a href="/accounts/login/">login</a> your account.</body>')
-    else:
-        return HttpResponse('Activation link has expired!')
-
 
 # @ensure_csrf_cookie
 def follow(request, following_id):
