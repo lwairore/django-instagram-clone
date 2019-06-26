0ab6682409378b0b485a57b74bf0045b582f4ab2
lwairore <kwairore@gmail.com>
2019-06-26 07:49:22 +0300

    Setup email configurations

--- instagram/settings.py
+++ instagram/settings.py
@@ -165,6 +165,12 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
 LOGIN_REDIRECT_URL = '/suggestions/'
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
